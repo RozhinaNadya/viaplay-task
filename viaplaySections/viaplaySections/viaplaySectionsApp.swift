bf2ce724fc26ef9749ecd302c2e63674ae8e402a
Nadya <zhuk.09@mail.ru>
2023-02-11 16:47:01 +0100

    Create a project

---
+++ viaplaySections/viaplaySections/viaplaySectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  viaplaySectionsApp.swift
+//  viaplaySections
+//
+//  Created by admin on 2023-02-11.
+//
+
+import SwiftUI
+
+@main
+struct viaplaySectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
